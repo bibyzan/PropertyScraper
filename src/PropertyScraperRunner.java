afdb8f2d6fad31e69ceb571b6794819ae6f52d25
bibyzan <benrasmonkey@gmail.com>
2017-07-01 17:08:35 -0400

    Added support for hamilton county website and writing to excel

--- src/PropertyScraperRunner.java
+++ src/PropertyScraperRunner.java
@@ -1,21 +1,28 @@
 import java.io.File;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import jxl.*;
 import jxl.write.*;
 import jxl.write.Number;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import javax.lang.model.element.Element;
+import javax.swing.*;
 
 public class PropertyScraperRunner {
 	private PropertyScraperWriter excelFile;
 	private WebDriver driver;
 
 	public PropertyScraperRunner() {
-		System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 		this.driver = new FirefoxDriver();
 		try {
 			this.excelFile = new PropertyScraperWriter();
@@ -26,42 +33,182 @@ public class PropertyScraperRunner {
 	}
 
 	public void close() {
-		driver.quit();
+		driver.close();
+	}
+
+	public void go() {
+		int hamilton = 0; int campbell = 0;int kenton = 0; int franklin = 0; int clermont = 0; int brown = 0; int other = 0; int butler = 0;
+
+		while (this.excelFile.hasNext()) {
+			try {
+				String county = this.getCounty(this.excelFile.currentZipCode());
+
+				if (county.equals("Hamilton")) {
+					runHamiltonProperty(this.excelFile.currentStreetNumber(), this.excelFile.currentStreetName());
+				} else {
+					this.excelFile.incrementRow();
+				}
+
+				/*if (county.equals("Hamilton")) {
+					hamilton++;
+				} else if (county.equals("Campbell")) {
+					campbell++;
+				} else if (county.equals("Brown")) {
+					brown++;
+				} else if (county.equals("Franklin")) {
+					franklin++;
+				} else if (county.equals("Clermont")) {
+					clermont++;
+				} else if (county.equals("Kenton")) {
+					System.out.println(excelFile.currentFullAddress());
+					kenton++;
+				} else if (county.equals("Butler")) {
+					butler++;
+				} else {
+					other++;
+					System.out.println("No function for getting info from: " + county);
+				}*/
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Error finding zip");
+				this.excelFile.incrementRow();
+			}
+
+		}
+
+		try {
+			this.excelFile.closeWriter();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Welp you almost made it");
+		}
+		this.close();
+	}
+
+	public String getCounty(String zipCode) throws Exception {
+		Document doc = Jsoup.connect("http://www.getzips.com/cgi-bin/ziplook.exe?What=1&Zip=" + zipCode + "&Submit=Look+It+Up").get();
+		return doc.select("table").get(2).select("td").get(6).text();
+	}
+
+	public void runKentonProperty(String streetNumber, String streetName) {
+		this.driver.get("http://kcor.org/");
+		this.driver.findElement(By.xpath("//*[@id=\"submit1\"]")).click();
+
+		String capcha = (String)JOptionPane.showInputDialog(
+				null,
+				"Please enter the CAPCHA",
+				"Property Scraper", JOptionPane.PLAIN_MESSAGE, null, null, "");
+
+		this.driver.findElement(By.xpath("//*[@id=\"input1\"]")).sendKeys(capcha);
+		this.driver.findElement(By.xpath("//*[@id=\"submit1\"]")).click();
+		this.driver.findElement(By.xpath("//*[@id=\"submit5\"]")).click();
+		this.driver.findElement(By.xpath("//*[@id=\"submit2\"]")).click();
+
+		this.driver.findElement(By.xpath("//*[@id=\"input2\"]")).sendKeys(streetNumber);
+		this.driver.findElement(By.xpath("//*[@id=\"input1\"]")).sendKeys(streetName);
+		this.driver.findElement(By.xpath("//*[@id=\"submit3\"]")).click();
+	}
+
+	public void runFranklinProperty(String streetNumber, String streetName) {
+
+
+	}
+
+	public void runButlerProperty(String streetNumber, String streetName) {
+
+	}
+
+	public void runWarrenProperty(String streetNumber, String streetName) {
+
+	}
+
+	public void runCampbellProperty(String streetNumber, String streetName) {
+
+	}
+
+	public void runBrownProperty(String streetNumber, String streetName) {
+
 	}
 
 	public void runHamiltonProperty(String houseNumber, String streetName) {
 		driver.get("http://wedge1.hcauditor.org");
 
+		try {
+			driver.findElement(By.xpath("//*[@id=\"house_number_low\"]")).sendKeys(houseNumber);
+			driver.findElement(By.xpath("//*[@id=\"street_name\"]")).sendKeys(streetName);
+			driver.findElement(By.xpath("//*[@id=\"search_by_street_address\"]/div[2]/button[1]")).click();
+			driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error filling out form");
+			this.excelFile.incrementRow();
+		}
 
-		driver.findElement(By.xpath("//*[@id=\"house_number_low\"]")).sendKeys(houseNumber);
-		driver.findElement(By.xpath("//*[@id=\"street_name\"]")).sendKeys(streetName);
-		driver.findElement(By.xpath("//*[@id=\"search_by_street_address\"]/div[2]/button[1]")).click();
+		try {
+			if (driver.findElement(By.xpath("//*[@id=\"search-results\"]/tbody/tr/td")).getText().equals("No data available in table")) {
+				System.out.println("No results, continuing...");
+				this.excelFile.incrementRow();
+				return;
+			}
+		} catch (Exception e) {
+			System.out.println("No empty table found trying to read info, continuing...");
+		}
 
-		String marketImprovementValue = driver.findElement(By.xpath("//*[@id=\"tax-credit-value-summary\"]/tbody/tr[9]/td[2]")).getText();
-		String marketLandValue = driver.findElement(By.xpath("//*[@id=\"tax-credit-value-summary\"]/tbody/tr[7]/td[2]")).getText();
-		String marketTotalValue = driver.findElement(By.xpath("//*[@id=\"tax-credit-value-summary\"]/tbody/tr[10]/td[2]")).getText();
-		String purchaseDate = driver.findElement(By.xpath("//*[@id=\"property_overview_wrapper\"]/table[1]/tbody/tr[6]/td[2]")).getText();
-		driver.findElement(By.xpath("//*[@id=\"parcel-tabs\"]/a[2]")).click();
+		try {
+			driver.findElement(By.xpath("//*[@id=\"search-results\"]/tbody/tr[1]")).click();
+			driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			System.out.println("No multiple results found, continuing...");
+		}
 
-		String stories = driver.findElement(By.xpath("//*[@id=\"3656845\"]/table[2]/tbody/tr[2]/td[2]")).getText() + " Story";
-		String squareFootage = driver.findElement(By.xpath("//*[@id=\"ajaxDiv\"]/table/tbody/tr[2]/td[2]")).getText();
-		String basementType = driver.findElement(By.xpath("//*[@id=\"3656845\"]/table[1]/tbody/tr[5]/td[2]")).getText();
-		String yearBuilt = driver.findElement(By.xpath("//*[@id=\"ajaxDiv\"]/table/tbody/tr[2]/td[3]")).getText();
+		try {
+			driver.findElement(By.xpath("//*[@id=\"parcel-tabs\"]/a[1]")).click();
+			String marketImprovementValue = driver.findElement(By.xpath("//*[@id=\"tax-credit-value-summary\"]/tbody/tr[9]/td[2]")).getText();
+			String marketLandValue = driver.findElement(By.xpath("//*[@id=\"tax-credit-value-summary\"]/tbody/tr[7]/td[2]")).getText();
+			String marketTotalValue = driver.findElement(By.xpath("//*[@id=\"tax-credit-value-summary\"]/tbody/tr[10]/td[2]")).getText();
+			String purchaseDate = driver.findElement(By.xpath("//*[@id=\"property_overview_wrapper\"]/table[1]/tbody/tr[6]/td[2]")).getText();
+			driver.findElement(By.xpath("//*[@id=\"parcel-tabs\"]/a[2]")).click();
 
-		excelFile.writeToSheet(marketImprovementValue, marketLandValue, marketTotalValue, purchaseDate, stories, squareFootage, basementType, yearBuilt);
-		System.out.println(marketImprovementValue);
-		System.out.println(marketLandValue);
-		System.out.println(marketTotalValue);
-		System.out.println(purchaseDate);
-		System.out.println(stories);
-		System.out.println(squareFootage);
-		System.out.println(basementType);
-		System.out.println(yearBuilt);
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			String stories = driver.findElements(By.xpath("//table[@class=\"residential datagrid left\"]/tbody/tr[2]/td[2]")).get(1).getText() + " Story";
+			String squareFootage = driver.findElement(By.xpath("//*[@id=\"ajaxDiv\"]/table/tbody/tr[2]/td[2]")).getText();
+			String basementType = driver.findElements(By.xpath("//table[@class=\"residential datagrid left\"]/tbody/tr[5]/td[2]")).get(0).getText(); //driver.findElement(By.xpath("//*[@id=\"ui-widget\"]/table[1]/tbody/tr[5]/td[2]")).getText();
+			String yearBuilt = driver.findElement(By.xpath("//*[@id=\"ajaxDiv\"]/table/tbody/tr[2]/td[3]")).getText();
+
+			this.excelFile.writeColumnCell(4, this.driver.getCurrentUrl());
+			this.excelFile.writeColumnCell(5, stories);
+			this.excelFile.writeColumnCell(8, squareFootage);
+			this.excelFile.writeColumnCell(11, basementType);
+			this.excelFile.writeColumnCell(12, yearBuilt);
+			this.excelFile.writeColumnCell(13, purchaseDate);
+			this.excelFile.writeColumnCell(15, marketTotalValue);
+			this.excelFile.writeColumnCell(16, marketLandValue);
+			this.excelFile.writeColumnCell(17, marketImprovementValue);
+			this.excelFile.incrementRow();
+		} catch (Exception e) {
+			System.out.println("Attempted searching: " + houseNumber + " " + streetName);
+			e.printStackTrace();
+			System.out.println("Continuing...");
+		}
+
 	}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		PropertyScraperRunner runner = new PropertyScraperRunner();
-		runner.runHamiltonProperty("10311","September Dr");
-		runner.close();
+		//runner.runKentonProperty("725","Dalton");
+		runner.go();
+		long elapsedTimeMillis = System.currentTimeMillis() - start;
+		float elapsedTimeSec = elapsedTimeMillis/1000F;
+		System.out.println(elapsedTimeSec);
+		//runner.runHamiltonProperty("2604", "Hackberry");
     }
 }
--- src/PropertyScraperWriter.java
+++ src/PropertyScraperWriter.java
@@ -15,15 +15,28 @@ import java.util.ArrayList;
 public class PropertyScraperWriter {
 	private int rowIndex;
 
+	private Workbook original;
 	private WritableWorkbook workbook;
 	private WritableSheet currentSheet;
 
 	public PropertyScraperWriter() throws Exception {
-		Workbook readWorkbook = Workbook.getWorkbook(new File("Property Template.xls"));
-		this.workbook = Workbook.createWorkbook(new File("scraped properties.xls"), readWorkbook);
-		readWorkbook.close();
+		this.original = Workbook.getWorkbook(new File("Property Template.xls"));
+		this.workbook = Workbook.createWorkbook(new File("scraped properties.xls"), this.original);
 		this.currentSheet = this.workbook.getSheet(0);
 		this.rowIndex = 1;
+	}
+
+	public void closeWriter() throws Exception {
+		this.workbook.write();
+		this.workbook.close();
+		this.original.close();
+}
+	public boolean hasNext() {
+		return !this.currentFullAddress().equals("");
+	}
+
+	public void incrementRow() {
+		this.rowIndex++;
 	}
 
 	public String currentFullAddress() {
@@ -32,9 +45,7 @@ public class PropertyScraperWriter {
 
 	public String currentStreetName() {
 		try {
-			String fullAddress = this.currentFullAddress();
-			int firstSpaceIndex = fullAddress.indexOf(" ");
-			return fullAddress.substring(firstSpaceIndex);
+			return this.currentFullAddress().split(" ")[1];
 		} catch (Exception e) {
 			System.out.println("Woops");
 			e.printStackTrace();
@@ -53,17 +64,29 @@ public class PropertyScraperWriter {
 		}
 	}
 
+	public String currentZipCode() {
+		return this.currentSheet.getCell(2, this.rowIndex).getContents();
+	}
+
+	public void writeColumnCell(int col, String text) throws Exception {
+		if (this.currentSheet.getWritableCell(col, this.rowIndex).getType() == CellType.LABEL) {
+			((Label)this.currentSheet.getWritableCell(col, this.rowIndex)).setString(text);
+		} else {
+			this.currentSheet.addCell(new Label(col, this.rowIndex, text));
+		}
+	}
+
 	public void writeToSheet(String marketImprovementValue, String marketLandValue, String marketTotalValue, String purchaseDate, String stories, String squareFootage, String basementType, String yearBuilt) {
 		try {
-			WritableCell tempCell = this.currentSheet.getWritableCell(5, this.rowIndex);
-
-
-			//((Blank)this.currentSheet.getWritableCell(5,this.rowIndex)).l
-			WritableCell cell = this.currentSheet.getWritableCell(0,0);
-			System.out.println(this.currentSheet.getWritableCell(0,0).getContents());
-			((Label)this.currentSheet.getWritableCell(5, this.rowIndex)).setString(stories);
-			this.workbook.write();
-			this.workbook.close();
+			this.writeColumnCell(5, stories);
+			this.writeColumnCell(8, squareFootage);
+			this.writeColumnCell(11, basementType);
+			this.writeColumnCell(12, yearBuilt);
+			this.writeColumnCell(13, purchaseDate);
+			this.writeColumnCell(15, marketTotalValue);
+			this.writeColumnCell(16, marketLandValue);
+			this.writeColumnCell(17, marketImprovementValue);
+			this.rowIndex++;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
